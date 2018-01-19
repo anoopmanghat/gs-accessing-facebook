5b4c5e277ece996dbcf258d95facec325ec32084
Anoop Manghat <manghat.anoop@gmail.com>
2018-01-18 22:53:09 -0500

    Initial commit + Social Constant

--- complete/src/main/java/hello/FacebookController.java
+++ complete/src/main/java/hello/FacebookController.java
@@ -1,9 +1,11 @@
 package hello;
 
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,26 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class FacebookController {
 
     private Facebook facebook;
     private ConnectionRepository connectionRepository;
 
-    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+    public FacebookController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
         this.connectionRepository = connectionRepository;
     }
 
     @GetMapping
     public String helloFacebook(Model model) {
-        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+    	Connection<Facebook> fbConnection = connectionRepository.findPrimaryConnection(Facebook.class);
+        if (fbConnection == null) {
             return "redirect:/connect/facebook";
         }
-
-        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        model.addAttribute("facebookProfile", facebook.fetchObject("me", User.class, SocialConstants.FB_PROFILE_FIELDS));
         PagedList<Post> feed = facebook.feedOperations().getFeed();
         model.addAttribute("feed", feed);
-        return "hello";
+        return "facebook";
     }
 
 }
